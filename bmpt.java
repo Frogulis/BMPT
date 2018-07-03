49c496c254929d33c8339d4646886de6b5e88b7f
Jamie Hoffmann <jamierhoffmann@gmail.com>
2018-07-03 13:11:47 +1000

    Skeleton files added for BMPT and Pixel

---
+++ bmpt.java
@@ -0,0 +1,24 @@
+class BMPT
+{
+    private static Dictionary<int, char[]> charSets;
+
+    public static void main(String[] args)
+    {
+        //whatever
+    }
+
+    public void printBitmap(Bitmap bmp)
+    {
+        //for each pixel do the thing
+    }
+
+    private void printPixel(Pixel curPixel)
+    {
+        //generate value then convert to character
+    }
+
+    private int pixelToValue(Pixel curPixel)
+    {
+        //some magic
+    }
+}
