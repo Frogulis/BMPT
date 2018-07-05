a2182b310e58b04dd9b9c1cb4c4bd05732c47a2d
Jamie Hoffmann <jamierhoffmann@gmail.com>
2018-07-05 23:42:55 +1000

    half working  - need to convert unsigned to 2's complement

--- BMPT.java
+++ BMPT.java
@@ -1,8 +1,14 @@
+import java.io.IOException;
+
 class BMPT
 {
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
+        BMPT a = new BMPT();
+        Bitmap b = new Bitmap();
+        b.loadFromFile("example.bmp");
+        a.printBitmap(b, 0);
     }
 
     public BMPT()
@@ -10,7 +16,7 @@ class BMPT
         //
     }
 
-    private static char[] getCharSets()
+    private static char[] getCharSet(String set)
     {
         char[] output = {'0', '1', '2', '3'};
         return output;
@@ -18,16 +24,33 @@ class BMPT
 
     public void printBitmap(Bitmap bmp, int termWidth)
     {
-        //for each pixel do the thing
+        int maxWidth;
+        if (termWidth == 0 || bmp.getImageWidth() < termWidth)
+        {
+            maxWidth = bmp.getImageWidth();
+        }
+        else
+        {
+            maxWidth = termWidth;
+        }
+        for (int y = 0; y < bmp.getImageHeight(); y++)
+        {
+            for (int x = 0; x < maxWidth; x++)
+            {
+                printPixel(bmp.getPixelAt(x, y));
+            }
+            System.out.print("\n");
+        }
     }
 
     private void printPixel(Pixel curPixel)
     {
-        //generate value then convert to character
+        char[] set = getCharSet("default");
+        System.out.print(set[pixelToValue(curPixel) / (256 / (set.length))]);
     }
 
     private int pixelToValue(Pixel curPixel)
     {
-        //some magic
+        return (curPixel.getR() + curPixel.getG() + curPixel.getB()) / 3;
     }
 }
