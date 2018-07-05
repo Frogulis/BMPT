76f42c01e21b75a043d515929e901e1065319ba4
Jamie Hoffmann <jamierhoffmann@gmail.com>
2018-07-06 01:47:35 +1000

    workingish
     - overflow issue still exists but it looks alright with photos
     - character set is crucial -- taking e.g. 64 chars will result in plentiful detail if i can order them right

--- BMPT.java
+++ BMPT.java
@@ -7,7 +7,7 @@ class BMPT
     {
         BMPT a = new BMPT();
         Bitmap b = new Bitmap();
-        b.loadFromFile("example.bmp");
+        b.loadFromFile(args[0]);
         a.printBitmap(b, 0);
     }
 
@@ -19,7 +19,7 @@ class BMPT
     private static char[] getCharSet(String set)
     {
         char[] output = {'.', ',', '-', '~',
-                         ':', ';', '!', '/',
+                         ':', ';', '!', '}',
                          '(', ']', '$', '#',
                          'R', 'B', 'N', 'M'};
         return output;
@@ -40,20 +40,56 @@ class BMPT
         {
             for (int x = 0; x < maxWidth; x++)
             {
-                printPixel(bmp.getPixelAt(x, y));
+                printPixel(bmp.getPixelAt(x, y), 1);
             }
             System.out.print("\n");
         }
     }
 
-    private void printPixel(Pixel curPixel)
+    private void printPixel(Pixel curPixel, int spacer)
     {
         char[] set = getCharSet("default");
-        System.out.print(set[pixelToValue(curPixel) / (256 / (set.length))]);
+        System.out.print(set[pixelToValue(curPixel) / (255 / (set.length))]);
+        for (int i = 0; i < spacer; i++)
+        {
+            System.out.print(" ");
+        }
     }
 
     private int pixelToValue(Pixel curPixel)
     {
-        return (curPixel.getR() + curPixel.getG() + curPixel.getB()) / 3;
+        int h = highest(curPixel.getR(), curPixel.getG(), curPixel.getB());
+        int l = lowest(curPixel.getR(), curPixel.getG(), curPixel.getB());
+        return (h + l) / 2;
+        //return h;
+    }
+
+    private int highest(int a, int b, int c)
+    {
+        if (a > b)
+            if (a > c)
+                return a;
+            else
+                return c;
+        else
+            if (b > c)
+                return b;
+            else
+                return c;
+    }
+
+    private int lowest(int a, int b, int c)
+    {
+        if (a < b)
+            if (a < c)
+                return a;
+            else
+                return c;
+        else
+            if (b < c)
+                return b;
+            else
+                return c;
     }
 }
+
